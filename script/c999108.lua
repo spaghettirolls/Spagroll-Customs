a25d5ffc522d911b933d4608f2d09206bc08e2e8
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-04-05 16:20:46 +0100

    update

--- script/c999108.lua
+++ script/c999108.lua
@@ -7,10 +7,18 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetCountLimit(1,id)
 	e1:SetCost(s.cost)
 	e1:SetTarget(s.target)
 	e1:SetOperation(s.activate)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_GRAVE)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	c:RegisterEffect(e2)
 end
 
 function s.tgfilter(c)
@@ -42,3 +50,25 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+function s.repfilter(c,tp)
+	return c:IsFaceup() and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE)
+		and c:IsSetCard(0x238C) and not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT)
+end
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsAbleToRemove() and eg:IsExists(s.repfilter,1,nil,tp) end
+	if Duel.SelectEffectYesNo(tp,e:GetHandler(),96) then
+		local g=eg:Filter(s.repfilter,nil,tp)
+		if #g==1 then
+			e:SetLabelObject(g:GetFirst())
+		else
+			Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESREPLACE)
+			local cg=g:Select(tp,1,1,nil)
+			e:SetLabelObject(cg:GetFirst())
+		end
+		Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+		return true
+	else return false end
+end
+function s.repval(e,c)
+	return c==e:GetLabelObject()
+end
