d3d980100617c75a55fc36e6c4a0a7b2b4ca650c
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-06-07 17:33:06 +0100

    Update c800901.lua

--- script/c800901.lua
+++ script/c800901.lua
@@ -9,6 +9,14 @@ function s.initial_effect(c)
 	e1:SetRange(LOCATION_HAND)
 	e1:SetCondition(s.notribcon)
 	c:RegisterEffect(e1)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SUMMON_SUCCESS)
+	e2:SetTarget(s.ntg)
+	e2:SetOperation(s.nop)
+	c:RegisterEffect(e2)
 end
 s.listed_names={15259703,id}
 function s.notribcon(e,c)
@@ -16,4 +24,28 @@ function s.notribcon(e,c)
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,15259703),tp,LOCATION_ONFIELD,0,1,nil)
+end
+function s.filter1(c)
+	return c:IsSetCard(SET_TOON) and c:IsSpellTrap() and c:IsAbleToHand()
+end
+function s.filter2(c)
+	return c:IsSetCard(SET_TOON) and c:IsMonster() and c:IsAbleToHand() and not c:IsCode(id)
+end
+function s.ntg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.filter1,tp,LOCATION_DECK,0,1,nil)
+		and Duel.IsExistingMatchingCard(s.filter2,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_DECK)
+end
+function s.nop(e,tp,eg,ep,ev,re,r,rp)
+	local g1=Duel.GetMatchingGroup(s.filter1,tp,LOCATION_DECK,0,nil)
+	local g2=Duel.GetMatchingGroup(s.filter2,tp,LOCATION_DECK,0,nil)
+	if #g1>0 and #g2>0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg1=g1:Select(tp,1,1,nil)
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local sg2=g2:Select(tp,1,1,nil)
+		sg1:Merge(sg2)
+		Duel.SendtoHand(sg1,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,sg1)
+	end
 end
