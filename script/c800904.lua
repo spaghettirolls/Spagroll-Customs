1ef29651f64118cd01ad64501abe95c4fcddbe01
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-06-23 17:02:40 +0100

    Update c800904.lua

--- script/c800904.lua
+++ script/c800904.lua
@@ -4,112 +4,68 @@
 local s,id=GetID()
 function s.initial_effect(c)
 	c:EnableReviveLimit()
-	local e1=Effect.CreateEffect(c)
+local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_QUICK_O)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCode(EVENT_CHAINING)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetCountLimit(1)
 	e1:SetCondition(s.eqcon)
 	e1:SetTarget(s.eqtg)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
-	aux.AddEREquipLimit(c,s.eqcon,function(ec,_,tp) return ec:IsControler(1-tp) end,s.equipop,e1)
-	--Change ATK/DEF
+	aux.AddEREquipLimit(c,nil,s.eqval,s.equipop,e1)
+	--Gains ATK/DEF equal to the equipped monsters'
 	local e2=Effect.CreateEffect(c)
 	e2:SetType(EFFECT_TYPE_SINGLE)
 	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e2:SetCode(EFFECT_UPDATE_ATTACK)
 	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_SET_ATTACK)
-	e2:SetCondition(s.adcon)
+	e2:SetCondition(function(e) return e:GetHandler():GetEquipGroup():FilterCount(s.eqgfilter,nil)>0 end)
 	e2:SetValue(s.atkval)
 	c:RegisterEffect(e2)
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e3:SetRange(LOCATION_MZONE)
-	e3:SetCode(EFFECT_SET_DEFENSE)
-	e3:SetCondition(s.adcon)
+	local e3=e2:Clone()
+	e3:SetCode(EFFECT_UPDATE_DEFENSE)
 	e3:SetValue(s.defval)
 	c:RegisterEffect(e3)
-	--damage
-	local e4=Effect.CreateEffect(c)
-	e4:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
-	e4:SetCode(EVENT_DAMAGE)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetCondition(s.damcon)
-	e4:SetOperation(s.damop)
-	c:RegisterEffect(e4)
 end
-s.listed_names={41426869}
-function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
-	local g=e:GetHandler():GetEquipGroup():Filter(s.eqfilter,nil)
-	return #g==0 and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,15259703),tp,LOCATION_ONFIELD,0,1,nil)
+function s.eqcon(e,tp,eg,ep,ev,re,r,rp,chk)
+	return Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,15259703),tp,LOCATION_ONFIELD,0,1,nil)
 end
 function s.eqfilter(c)
-	return c:GetFlagEffect(id)~=0
+	return c:IsFaceup() and c:IsType(TYPE_EFFECT) and c:IsAbleToChangeControler()
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and chkc:IsAbleToChangeControler() end
+	if chkc then return chkc:IsLocation(LOCATION_MZONE) and chkc:IsControler(1-tp) and s.eqfilter(chkc) end
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_SZONE)>0
-		and Duel.IsExistingTarget(Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,nil) end
+		and Duel.IsExistingTarget(s.eqfilter,tp,0,LOCATION_MZONE,1,nil) end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToChangeControler,tp,0,LOCATION_MZONE,1,1,nil)
+	local g=Duel.SelectTarget(tp,s.eqfilter,tp,0,LOCATION_MZONE,1,1,nil)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,g,1,0,0)
-end
-function s.equipop(c,e,tp,tc)
-	if not c:EquipByEffectAndLimitRegister(e,tp,tc,id) then return end
-	--substitute
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_EQUIP)
-	e1:SetCode(EFFECT_DESTROY_SUBSTITUTE)
-	e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE+EFFECT_FLAG_IGNORE_IMMUNE)
-	e1:SetReset(RESET_EVENT|RESETS_STANDARD)
-	e1:SetValue(s.repval)
-	tc:RegisterEffect(e1)
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) and tc:IsMonster() and tc:IsControler(1-tp) and s.eqcon(e,tp,eg,ep,ev,re,r,rp) then
+	if tc:IsRelateToEffect(e) and s.eqfilter(tc) and tc:IsControler(1-tp) then
 		s.equipop(c,e,tp,tc)
 	end
 end
-function s.repval(e,re,r,rp)
-	return r&REASON_BATTLE~=0
+function s.eqval(ec,c,tp)
+	return ec:IsControler(1-tp) and ec:IsType(TYPE_EFFECT)
 end
-function s.damcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
-	return #g>0 and ep==tp and r&REASON_BATTLE~=0
-		and (Duel.GetAttacker()==e:GetHandler() or Duel.GetAttackTarget()==e:GetHandler())
+function s.equipop(c,e,tp,tc)
+	c:EquipByEffectAndLimitRegister(e,tp,tc,id)
 end
-function s.damop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Damage(1-tp,ev,REASON_EFFECT)
-end
-function s.adcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
-	return #g>0
+function s.eqgfilter(c)
+	return c:IsFaceup() and c:HasFlagEffect(id)
 end
 function s.atkval(e,c)
-	local c=e:GetHandler()
-	local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
-	local atk=g:GetFirst():GetTextAttack()
-	if g:GetFirst():IsFacedown() or g:GetFirst():GetOriginalType()&TYPE_MONSTER==0 or atk<0 then
-		return 0
-	else
-		return atk
-	end
+	local g=c:GetEquipGroup():Match(s.eqgfilter,nil):Match(function(c) return c:GetTextAttack()>0 end,nil)
+	return g:GetSum(Card.GetTextAttack)
 end
 function s.defval(e,c)
-	local c=e:GetHandler()
-	local g=c:GetEquipGroup():Filter(s.eqfilter,nil)
-	local def=g:GetFirst():GetTextDefense()
-	if g:GetFirst():IsFacedown() or g:GetFirst():GetOriginalType()&TYPE_MONSTER==0 or def<0 then
-		return 0
-	else
-		return def
-	end
+	local g=c:GetEquipGroup():Match(s.eqgfilter,nil):Match(function(c) return c:GetTextDefense()>0 end,nil)
+	return g:GetSum(Card.GetTextDefense)
 end
