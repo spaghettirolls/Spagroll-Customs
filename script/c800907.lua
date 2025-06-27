76af4a688f1e7f049ab2955882a084ab35acc45c
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-06-27 16:14:16 +0100

    Update c800907.lua

--- script/c800907.lua
+++ script/c800907.lua
@@ -3,16 +3,14 @@
 
 local s,id=GetID()
 function s.initial_effect(c)
-local e1=Effect.CreateEffect(c)
-e1:SetDescription(aux.Stringid(id,0))
-e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-e1:SetType(EFFECT_TYPE_ACTIVATE)
-e1:SetCode(EVENT_FREE_CHAIN)
-e1:SetHintTiming(0,TIMING_END_PHASE)
-e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
-e1:SetTarget(s.addtg)
-e1:SetOperation(s.addop)
-c:RegisterEffect(e1)
+	local e1=Effect.CreateEffect(c)
+	e1:SetCategory(CATEGORY_TOHAND)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	e1:SetTarget(s.target)
+	e1:SetOperation(s.activate)
+	c:RegisterEffect(e1)
 local params = {fusfilter=aux.FilterBoolFunction(Card.IsSetCard,SET_TOON),matfilter=aux.FALSE,extrafil=s.fextra,
 extraop=Fusion.BanishMaterial,gc=Fusion.ForcedHandler,extratg=s.extratarget}
 local e2=Effect.CreateEffect(c)
@@ -25,7 +23,23 @@ e2:SetOperation(Fusion.SummonEffOP(params))
 c:RegisterEffect(e2)
 end
 s.listed_series={SET_TOON}
-
+function s.filter(c)
+	return c:IsSetCard(0x98) and c:IsMonster() and c:IsAbleToHand()
+end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.filter(chkc) end
+	if chk==0 then return Duel.IsExistingTarget(s.filter,tp,LOCATION_GRAVE,0,2,nil) end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_GRAVE,0,2,2,nil)
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,g,2,0,0)
+end
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+	local g=Duel.GetChainInfo(0,CHAININFO_TARGET_CARDS)
+	local sg=g:Filter(Card.IsRelateToEffect,nil,e)
+	if #sg>0 then
+		Duel.SendtoHand(sg,nil,REASON_EFFECT)
+	end
+end
 function s.fextra(e,tp,mg)
 	if not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
 		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
@@ -35,22 +49,4 @@ end
 function s.extratarget(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
 	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),0,tp,LOCATION_GRAVE)
-end
-
-function s.addfilter(c)
-	return c:IsSetCard(0x98) and c:IsMonster() and c:IsAbleToHand()
-end
-function s.addtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	local g=Duel.GetMatchingGroup(s.addfilter,tp,LOCATION_GRAVE,0,nil)
-	if chk==0 then return aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,0) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,2,tp,LOCATION_GRAVE)
-end
-function s.addop(e,tp,eg,ep,ev,re,r,rp)
-		local g=Duel.GetMatchingGroup(s.addfilter,tp,LOCATION_GRAVE,0,nil)
-		if #g<2 then return end
-		local thg=aux.SelectUnselectGroup(g,e,tp,2,2,aux.dncheck,1,tp,HINTMSG_ATOHAND)
-		if #thg>0 then
-			Duel.SendtoHand(thg,nil,REASON_EFFECT)
-			Duel.ConfirmCards(1-tp,thg)
-	end
 end
