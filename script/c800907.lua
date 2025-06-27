9feaa07cf416b12899a1eff015aa19eeefeb334f
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-06-27 16:19:15 +0100

    Update c800907.lua

--- script/c800907.lua
+++ script/c800907.lua
@@ -1,6 +1,5 @@
 --Toon Recovery
 --Scipted by Beanbag
-
 local s,id=GetID()
 function s.initial_effect(c)
 	local e1=Effect.CreateEffect(c)
@@ -9,32 +8,11 @@ function s.initial_effect(c)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
 	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
 	e1:SetCode(EVENT_FREE_CHAIN)
-	e1:SetCountLimit(1,id,EFFECT_COUNT_CODE_OATH)
 	e1:SetTarget(s.thtg)
 	e1:SetOperation(s.thop)
 	c:RegisterEffect(e1)
-local params = {fusfilter=aux.FilterBoolFunction(Card.IsSetCard,SET_TOON),matfilter=aux.FALSE,extrafil=s.fextra,
-extraop=Fusion.BanishMaterial,gc=Fusion.ForcedHandler,extratg=s.extratarget}
-local e2=Effect.CreateEffect(c)
-e2:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_FUSION_SUMMON)
-e2:SetType(EFFECT_TYPE_IGNITION)
-e2:SetRange(LOCATION_GRAVE)
-e2:SetCountLimit(1,{id,0})
-e2:SetTarget(Fusion.SummonEffTG(params))
-e2:SetOperation(Fusion.SummonEffOP(params))
-c:RegisterEffect(e2)
 end
-s.listed_series={SET_TOON}
-function s.fextra(e,tp,mg)
-	if not Duel.IsPlayerAffectedByEffect(tp,CARD_SPIRIT_ELIMINATION) then
-		return Duel.GetMatchingGroup(Fusion.IsMonsterFilter(Card.IsAbleToRemove),tp,LOCATION_GRAVE,0,nil)
-	end
-	return nil
-end
-function s.extratarget(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),0,tp,LOCATION_GRAVE)
-end
+
 function s.thfilter(c,e)
 	return c:IsSetCard(0x98) and c:IsMonster() and c:IsAbleToHand() and c:IsCanBeEffectTarget(e)
 end
