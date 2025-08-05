7c41852bed54711ee324710684da8881b86106a2
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-08-05 17:24:58 +0100

    Create c999012.lua

---
+++ script/c999012.lua
@@ -0,0 +1,50 @@
+-- Holysphere Finality
+--Scrited by Beanbag
+
+local s,id=GetID()
+function s.initial_effect(c)
+	aux.AddPersistentProcedure(c,0,s.filter)
+	--cannot be destroyed effect
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT+EFFECT_INDESTRUCTABLE_BATTLE)
+	e1:SetRange(LOCATION_SZONE)
+	e1:SetTargetRange(LOCATION_MZONE,LOCATION_MZONE)
+	e1:SetTarget(aux.PersistentTargetFilter)
+	e1:SetValue(1)
+	c:RegisterEffect(e1)
+    local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_HAND_LIMIT)
+	e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetTargetRange(0,1)
+	e2:SetValue(3)
+	c:RegisterEffect(e2)
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_CONTINUOUS+EFFECT_TYPE_SINGLE)
+	e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetTarget(s.desreptg)
+	c:RegisterEffect(e3)
+end
+
+function s.filter(c)
+	return c:IsFaceup() and c:IsSetCard(0x270F) and c:IsRitualMonster()
+end
+
+function s.repfilter(c)
+	return c:IsSetCard(0x270F) and c:IsAbleToRemoveAsCost() and aux.SpElimFilter(c,true)
+end
+function s.desreptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return not c:IsReason(REASON_REPLACE) and c:IsReason(REASON_EFFECT)
+		and Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,nil) end
+	if Duel.SelectEffectYesNo(tp,c,96) then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+		local g=Duel.SelectMatchingCard(tp,s.repfilter,tp,LOCATION_MZONE|LOCATION_GRAVE,0,1,1,nil)
+		Duel.Remove(g,POS_FACEUP,REASON_COST)
+		return true
+	else return false end
+end
