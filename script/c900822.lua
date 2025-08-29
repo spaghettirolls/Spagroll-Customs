b1e64112fdb838bda3443b4cb38ab43d1d4400e9
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-08-29 15:11:23 +0100

    update

--- script/c900822.lua
+++ script/c900822.lua
@@ -24,17 +24,13 @@ function s.initial_effect(c)
 	e2:SetOperation(s.setop)
 	c:RegisterEffect(e2)
 end
---Activation & Trap Monster summon
-function s.tgfilter(c)
-	return c:IsSetCard(0x3a5) and c:IsAbleToGrave()
-end
-function s.exfilter(c)
-	return c:IsSetCard(0x3a5) and c:IsAbleToGrave() and (c:IsType(TYPE_FUSION) or c:IsType(TYPE_LINK))
+function s.filter(c,e)
+	return c:IsMonster() and c:IsSetCard(0x3D4)
 end
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then
 		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-			and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x3a5,TYPE_EFFECT+TYPE_TRAP,1500,1500,4,RACE_FIEND,ATTRIBUTE_DARK)
+			and Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x21,TYPE_EFFECT+TYPE_TRAP,1500,1500,4,RACE_FIEND,ATTRIBUTE_DARK)
 	end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,0)
 end
@@ -42,17 +38,17 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	if not c:IsRelateToEffect(e) then return end
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 
-		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x3a5,TYPE_EFFECT+TYPE_TRAP,1500,1500,4,RACE_FIEND,ATTRIBUTE_DARK) then return end
+		or not Duel.IsPlayerCanSpecialSummonMonster(tp,id,0x21,TYPE_EFFECT+TYPE_TRAP,1500,1500,4,RACE_FIEND,ATTRIBUTE_DARK) then return end
 	c:AddMonsterAttribute(TYPE_EFFECT+TYPE_TRAP)
-	c:MonsterAttributeComplete()
-	Duel.SpecialSummon(c,SUMMON_TYPE_SPECIAL,tp,tp,true,false,POS_FACEUP)
-	--Send 1 Poltiquette monster from Deck or Extra to GY
-	local g=Duel.SelectMatchingCard(tp,aux.NecroValleyFilter(function(c) 
-		return s.tgfilter(c) or s.exfilter(c) 
-	end),tp,LOCATION_DECK+LOCATION_EXTRA,0,1,1,nil)
-	if #g>0 then
-		Duel.SendtoGrave(g,REASON_EFFECT)
-	end
+	Duel.SpecialSummonStep(c,1,tp,tp,true,false,POS_FACEUP)
+	c:AddMonsterAttributeComplete()
+    local g=Duel.GetMatchingGroup(s.filter,tp,LOCATION_DECK+LOCATION_EXTRA,0,nil)
+	if #g>0 and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+		Duel.BreakEffect()
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOGRAVE)
+		local dg=g:Select(tp,1,1,nil)
+		Duel.HintSelection(dg)
+		Duel.SendtoGrave(dg,REASON_EFFECT)
 	--Damage effects while on field
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
@@ -67,6 +63,9 @@ function s.activate(e,tp,eg,ep,ev,re,r,rp)
 	e2:SetValue(s.damval2)
 	c:RegisterEffect(e2)
 end
+end
+end
+
 --Half damage to controller (cannot apply twice)
 function s.damval1(e,re,val,r,rp,rc)
 	if bit.band(r,REASON_EFFECT+REASON_BATTLE)~=0 then
@@ -107,5 +106,4 @@ function s.setop(e,tp,eg,ep,ev,re,r,rp)
 		e1:SetValue(LOCATION_REMOVED)
 		c:RegisterEffect(e1)
 	end
-end
 end
