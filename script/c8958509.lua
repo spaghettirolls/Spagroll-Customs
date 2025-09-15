40dbe020f3a315fe20a0d78cdea3d8d2ed2aa7f7
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-09-15 17:00:35 +0100

    Update c8958509.lua

--- script/c8958509.lua
+++ script/c8958509.lua
@@ -4,47 +4,39 @@ function s.initial_effect(c)
 	--Synchro Summon
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x30),1,1,Synchro.NonTunerEx(Card.IsSetCard,0x30),1,99)
 	c:EnableReviveLimit()
-		local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
+local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCondition(s.eqcon)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_F)
 	e1:SetTarget(s.eqtg)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
-	aux.AddEREquipLimit(c,nil,aux.FilterBoolFunction(Card.IsMonster),s.equipop,e1)
-end
-function s.eqcon(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local tc=c:GetBattleTarget()
-	return c:IsRelateToBattle() and c:IsFaceup() and tc:IsLocation(LOCATION_GRAVE) and tc:IsMonster() and tc:IsReason(REASON_BATTLE) 
-		and not tc:IsForbidden()
+	aux.AddEREquipLimit(c,nil,function(ec,_,tp) return ec:GetOriginalType()&TYPE_MONSTER>0 end,Card.EquipByEffectAndLimitRegister,e1)
 end
 function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return true end
-	local tc=e:GetHandler():GetBattleTarget()
-	Duel.SetTargetCard(tc)
-	Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,tc,1,0,0)
-end
-function s.equipop(c,e,tp,tc)
-	local atk=tc:GetTextAttack()
-	if not c:EquipByEffectAndLimitRegister(e,tp,tc) then return end
-	if atk<0 then atk=0 end
-	if atk>0 then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_EQUIP)
-		e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE+EFFECT_FLAG_OWNER_RELATE)
-		e2:SetCode(EFFECT_UPDATE_ATTACK)
-		e2:SetReset(RESET_EVENT|RESETS_STANDARD)
-		e2:SetValue(atk)
-		tc:RegisterEffect(e2)
+	local bc=e:GetHandler():GetBattleTarget()
+	if bc then
+		Duel.SetTargetCard(bc)
+		Duel.SetOperationInfo(0,CATEGORY_EQUIP,bc,1,0,0)
+		if bc:IsLocation(LOCATION_GRAVE) then
+			Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,bc,1,0,0)
+		end
 	end
 end
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
 	local c=e:GetHandler()
-	local tc=Duel.GetFirstTarget()
-	if tc and tc:IsRelateToEffect(e) then
-		s.equipop(c,e,tp,tc)
+	local bc=c:GetBattleTarget()
+	if not bc or c:IsStatus(STATUS_BATTLE_DESTROYED) then return end
+	if c:IsFaceup() and c:IsRelateToEffect(e) and bc and bc:IsRelateToEffect(e) and bc:IsLocation(LOCATION_SZONE|LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED) then
+		c:EquipByEffectAndLimitRegister(e,tp,bc)
 	end
+end
+function s.eqcheck(e,tp,eg,ep,ev,re,r,rp)
+	if e:GetLabelObject() then e:GetLabelObject():DeleteGroup() end
+	local g=e:GetHandler():GetEquipGroup()
+	g:KeepAlive()
+	e:SetLabelObject(g)
 end
