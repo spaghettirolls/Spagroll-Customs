8671725b9b1ac341fb9720b0ff4573efa8358b05
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-09-15 17:11:44 +0100

    Update c8958509.lua

--- script/c8958509.lua
+++ script/c8958509.lua
@@ -4,39 +4,65 @@ function s.initial_effect(c)
 	--Synchro Summon
 	Synchro.AddProcedure(c,aux.FilterBoolFunctionEx(Card.IsSetCard,0x30),1,1,aux.FilterBoolFunctionEx(Card.IsSetCard,0x30),1,99)
 	c:EnableReviveLimit()
-local e1=Effect.CreateEffect(c)
+--Custom Vylon-style monster
+local s,id=GetID()
+function s.initial_effect(c)
+	--Equip destroyed monster
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
-	e1:SetCode(EVENT_BATTLE_DESTROYING)
-	e1:SetProperty(EFFECT_FLAG_DELAY)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_BATTLE_DESTROYING)
+	e1:SetCondition(aux.bdocon) -- standard battle destruction condition
 	e1:SetTarget(s.eqtg)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
-	aux.AddEREquipLimit(c,nil,function(ec,_,tp) return ec:GetOriginalType()&TYPE_MONSTER>0 end,Card.EquipByEffectAndLimitRegister,e1)
 end
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return true end
-	local bc=e:GetHandler():GetBattleTarget()
-	if bc then
-		Duel.SetTargetCard(bc)
-		Duel.SetOperationInfo(0,CATEGORY_EQUIP,bc,1,0,0)
-		if bc:IsLocation(LOCATION_GRAVE) then
-			Duel.SetOperationInfo(0,CATEGORY_LEAVE_GRAVE,bc,1,0,0)
-		end
-	end
-end
-function s.eqop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)<=0 then return end
+
+-- Target the monster destroyed by battle
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
 	local c=e:GetHandler()
-	local bc=c:GetBattleTarget()
-	if not bc or c:IsStatus(STATUS_BATTLE_DESTROYED) then return end
-	if c:IsFaceup() and c:IsRelateToEffect(e) and bc and bc:IsRelateToEffect(e) and bc:IsLocation(LOCATION_SZONE|LOCATION_HAND|LOCATION_GRAVE|LOCATION_REMOVED) then
-		c:EquipByEffectAndLimitRegister(e,tp,bc)
-	end
+	local tc=c:GetBattleTarget()
+	if chk==0 then return tc and tc:IsRelateToBattle() end
+	Duel.SetTargetCard(tc)
+	Duel.SetOperationInfo(0,CATEGORY_EQUIP,tc,1,0,0)
 end
-function s.eqcheck(e,tp,eg,ep,ev,re,r,rp)
-	if e:GetLabelObject() then e:GetLabelObject():DeleteGroup() end
-	local g=e:GetHandler():GetEquipGroup()
-	g:KeepAlive()
-	e:SetLabelObject(g)
+
+-- Equip and apply ATK/DEF boost + treat as Vylon
+function s.eqop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	local tc=Duel.GetFirstTarget()
+	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
+	if tc and tc:IsRelateToEffect(e) and tc:IsRelateToBattle() then
+		if not Duel.Equip(tp,tc,c) then return end
+		
+		-- Equip limit (the card can only be equipped to this monster)
+		local e1=Effect.CreateEffect(c)
+		e1:SetType(EFFECT_TYPE_SINGLE)
+		e1:SetCode(EFFECT_EQUIP_LIMIT)
+		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+		e1:SetValue(function(e,c) return c==e:GetOwner() end)
+		tc:RegisterEffect(e1)
+
+		-- ATK/DEF boost
+		local e2=Effect.CreateEffect(c)
+		e2:SetType(EFFECT_TYPE_EQUIP)
+		e2:SetCode(EFFECT_UPDATE_ATTACK)
+		e2:SetValue(500)
+		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e2)
+
+		local e3=e2:Clone()
+		e3:SetCode(EFFECT_UPDATE_DEFENSE)
+		tc:RegisterEffect(e3)
+
+		-- Treated as "Vylon"
+		local e4=Effect.CreateEffect(c)
+		e4:SetType(EFFECT_TYPE_EQUIP)
+		e4:SetCode(EFFECT_ADD_SETCODE)
+		e4:SetValue(0x30) -- Vylon archetype setcode
+		e4:SetReset(RESET_EVENT+RESETS_STANDARD)
+		tc:RegisterEffect(e4)
+	end
 end
