2f9c534054a7749b5f75178d72981e4cc3131154
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-09-15 17:18:39 +0100

    Update c8958509.lua

--- script/c8958509.lua
+++ script/c8958509.lua
@@ -9,40 +9,38 @@ function s.initial_effect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_EQUIP)
 	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET+EFFECT_FLAG_DELAY)
+	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_DAMAGE_STEP)
 	e1:SetCode(EVENT_BATTLE_DESTROYING)
-	e1:SetCondition(aux.bdocon) -- standard battle destruction condition
 	e1:SetTarget(s.eqtg)
 	e1:SetOperation(s.eqop)
 	c:RegisterEffect(e1)
 end
 
--- Target the monster destroyed by battle
-function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+--Filter: can only equip destroyed monsters
+function s.eqtg(e,tp,eg,ep,ev,re,r,rp,chk)
 	local c=e:GetHandler()
-	local tc=c:GetBattleTarget()
-	if chk==0 then return tc and tc:IsRelateToBattle() end
+	local tc=e:GetHandler():GetBattleTarget()
+	if chk==0 then return tc and tc:IsLocation(LOCATION_GRAVE) and tc:IsMonster() end
 	Duel.SetTargetCard(tc)
 	Duel.SetOperationInfo(0,CATEGORY_EQUIP,tc,1,0,0)
 end
 
--- Equip and apply ATK/DEF boost + treat as Vylon
 function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 	local tc=Duel.GetFirstTarget()
-	if not (c:IsRelateToEffect(e) and c:IsFaceup()) then return end
-	if tc and tc:IsRelateToEffect(e) and tc:IsRelateToBattle() then
-		if not Duel.Equip(tp,tc,c) then return end
+	if not c:IsRelateToEffect(e) or not c:IsFaceup() then return end
+	if tc and tc:IsRelateToEffect(e) and tc:IsControler(1-tp) and tc:IsLocation(LOCATION_GRAVE) then
+		if not Duel.Equip(tp,tc,c,false) then return end
 		
-		-- Equip limit (the card can only be equipped to this monster)
+		--Limit equip
 		local e1=Effect.CreateEffect(c)
 		e1:SetType(EFFECT_TYPE_SINGLE)
 		e1:SetCode(EFFECT_EQUIP_LIMIT)
 		e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-		e1:SetValue(function(e,c) return c==e:GetOwner() end)
+		e1:SetValue(function(e,c) return c==e:GetHandler() end)
 		tc:RegisterEffect(e1)
 
-		-- ATK/DEF boost
+		--ATK boost
 		local e2=Effect.CreateEffect(c)
 		e2:SetType(EFFECT_TYPE_EQUIP)
 		e2:SetCode(EFFECT_UPDATE_ATTACK)
@@ -50,16 +48,21 @@ function s.eqop(e,tp,eg,ep,ev,re,r,rp)
 		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e2)
 
-		local e3=e2:Clone()
+		--DEF boost
+		local e3=Effect.CreateEffect(c)
+		e3:SetType(EFFECT_TYPE_EQUIP)
 		e3:SetCode(EFFECT_UPDATE_DEFENSE)
+		e3:SetValue(500)
+		e3:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e3)
 
-		-- Treated as "Vylon"
+		--Treat as "Vylon"
 		local e4=Effect.CreateEffect(c)
 		e4:SetType(EFFECT_TYPE_EQUIP)
 		e4:SetCode(EFFECT_ADD_SETCODE)
-		e4:SetValue(0x30) -- Vylon archetype setcode
+		e4:SetValue(0x30) -- 0x30 is "Vylon"
 		e4:SetReset(RESET_EVENT+RESETS_STANDARD)
 		tc:RegisterEffect(e4)
 	end
 end
+
