d85c66f0cde96e272bbae5d379a24932d766205d
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-11-28 15:49:16 +0000

    Update c900500.lua

--- script/c900500.lua
+++ script/c900500.lua
@@ -1,48 +1,58 @@
---Fisherman of the Deepest Blue
 local s,id=GetID()
-
 function s.initial_effect(c)
- 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetRange(LOCATION_HAND)
-	e1:SetCountLimit(1,id)
-	e1:SetTarget(s.pltg)
-	e1:SetOperation(s.plop)
-	c:RegisterEffect(e1)
+    --Special Summon + place "Umi"
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.spcon)
+    e1:SetTarget(s.sptg)
+    e1:SetOperation(s.spop)
+    c:RegisterEffect(e1)
 end
-function s.plfilter(c,tp)
-	return c:IsCode(22702055)
+
+-- There must be space to Special Summon from hand
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 end
-function s.pltg(e,tp,eg,ep,ev,re,r,rp,chk)
-	local c=e:GetHandler()
-	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_FZONE)>0
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
-		and Duel.IsExistingMatchingCard(s.plfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,nil,tp) end
-	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,c,1,tp,0)
+
+function s.umifilter(c,tp)
+    return c:IsCode(22702055) -- "Umi"
+        and (c:IsAbleToGrave() or c:IsAbleToHand() or c:IsAbleToDeck())
 end
-function s.plop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if Duel.GetLocationCount(tp,LOCATION_SZONE)>0 then
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
-		local tc=Duel.SelectMatchingCard(tp,s.plfilter,tp,LOCATION_DECK+LOCATION_HAND+LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
-		if tc and Duel.MoveToField(tc,tp,tp,LOCATION_FZONE,POS_FACEUP,true) and c:IsRelateToEffect(e) then
-			Duel.BreakEffect()
-			Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-		end
-	end
-	--Cannot Special Summon from the Extra Deck for the rest of this turn, except Insect monsters
-	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,2))
-	e1:SetType(EFFECT_TYPE_FIELD)
-	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_CLIENT_HINT)
-	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-	e1:SetTargetRange(1,0)
-	e1:SetTarget(function(e,c) return c:IsLocation(LOCATION_EXTRA) and not c:IsRace(RACE_INSECT) end)
-	e1:SetReset(RESET_PHASE|PHASE_END)
-	Duel.RegisterEffect(e1,tp)
-	--Clock Lizard check
-	aux.addTempLizardCheck(c,tp,function(e,c) return not c:IsOriginalRace(RACE_INSECT) end)
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingMatchingCard(s.umifilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,tp)
+            and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false)
+    end
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    -- Pick Umi from hand/Deck/GY
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TOFIELD)
+    local tc=Duel.SelectMatchingCard(tp,s.umifilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,tp):GetFirst()
+    if not tc then return end
+    
+    -- Choose which Field Zone to place it to
+    Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1)) 
+    local fz=Duel.SelectOption(tp,aux.Stringid(id,2),aux.Stringid(id,3)) -- 0 = yours, 1 = opponent's
+    local p=(fz==0 and tp or 1-tp)
+    
+    -- Clear existing Field Spell on that side
+    local fc=Duel.GetFieldCard(p,LOCATION_FZONE,0)
+    if fc then
+        Duel.SendtoGrave(fc,REASON_RULE)
+    end
+
+    -- Move Umi to chosen Field Zone
+    Duel.MoveToField(tc,tp,p,LOCATION_FZONE,POS_FACEUP,true)
+
+    -- Special Summon this card
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+    end
 end
