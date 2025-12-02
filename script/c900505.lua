cb1aff585620134ab58df807214635033b9ce445
Spagroll-Customs <Joecollins@hotmail.co.uk>
2025-12-02 09:40:31 +0000

    Update c900505.lua

--- script/c900505.lua
+++ script/c900505.lua
@@ -5,21 +5,24 @@ Link.AddProcedure(c, s.matfilter, 1, 1)
 c:EnableReviveLimit()
 
 
--- Name change while on the field (MZONE)
+local PHANTASM_CODE = 566490609 -- numeric card ID for "Phantasm Spiral Dragon"
+
+
+-- Name change while on the field (MZONE) with hint
 local e1=Effect.CreateEffect(c)
 e1:SetType(EFFECT_TYPE_SINGLE)
-e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE+EFFECT_FLAG_CLIENT_HINT)
 e1:SetCode(EFFECT_CHANGE_CODE)
 e1:SetRange(LOCATION_MZONE)
--- Use the target card's code here. If an engine constant exists, use it (e.g. CARD_PHANTASM_SPIRAL_DRAGON)
--- Otherwise replace the value with the numeric card ID: e1:SetValue(12345678)
-e1:SetValue(CARD_PHANTASM_SPIRAL_DRAGON)
+e1:SetValue(PHANTASM_CODE)
+e1:SetDescription(aux.Stringid(id,0)) -- hint message ID
 c:RegisterEffect(e1)
 
 
 -- Name change while in the GY
 local e2=e1:Clone()
 e2:SetRange(LOCATION_GRAVE)
+e2:SetDescription(aux.Stringid(id,1)) -- optional different hint for GY
 c:RegisterEffect(e2)
 end
 
