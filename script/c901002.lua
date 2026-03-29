0ced639eabdb4fb7a6985cbebb2d7f1cc807d2d9
Spagroll-Customs <Joecollins@hotmail.co.uk>
2026-03-29 13:26:06 +0100

    update

---
+++ script/c901002.lua
@@ -0,0 +1,148 @@
+--Psychic Rose Thorn
+local s,id=GetID()
+function s.initial_effect(c)
+	--This card is also treated as a Psychic monster
+	local e0=Effect.CreateEffect(c)
+	e0:SetType(EFFECT_TYPE_SINGLE)
+	e0:SetCode(EFFECT_ADD_SETCODE)
+	e0:SetValue(0x5f1)
+	c:RegisterEffect(e0)
+
+	--Custom Activity Counter (Psychic/Plant only)
+	Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,function(c)
+		return c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT)
+	end)
+
+	--Special Summon from hand
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_IGNITION)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,id)
+	e1:SetCondition(s.hspcon)
+	e1:SetTarget(s.hsptg)
+	e1:SetOperation(s.hspop)
+	c:RegisterEffect(e1)
+
+	--Tribute effect
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCountLimit(1,id+1)
+	e2:SetCost(s.tgcost)
+	e2:SetTarget(s.tgtg)
+	e2:SetOperation(s.tgop)
+	c:RegisterEffect(e2)
+
+	--Graveyard replacement effect
+	local e3=Effect.CreateEffect(c)
+	e3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e3:SetCode(EFFECT_DESTROY_REPLACE)
+	e3:SetRange(LOCATION_GRAVE)
+	e3:SetCountLimit(1,id+2)
+	e3:SetTarget(s.reptg)
+	e3:SetValue(s.repval)
+	e3:SetOperation(s.repop)
+	c:RegisterEffect(e3)
+end
+
+--------------------------------------------------
+-- Hand Special Summon
+--------------------------------------------------
+function s.cfilter(c)
+	return c:IsFaceup() and c:IsSetCard(0x1A0A)
+end
+
+function s.hspcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.IsExistingMatchingCard(s.cfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.hsptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
+end
+
+function s.hspop(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	if c:IsRelateToEffect(e) then
+		Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+--------------------------------------------------
+-- Tribute Special Summon
+--------------------------------------------------
+function s.tgcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.CheckReleaseGroup(tp,s.cfilter,1,nil) end
+	local g=Duel.SelectReleaseGroup(tp,s.cfilter,1,1,nil)
+	Duel.Release(g,REASON_COST)
+end
+
+function s.spfilter(c,e,tp)
+	return c:IsSetCard(0x1A0A) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,math.min(2,ft),tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local ft=Duel.GetLocationCount(tp,LOCATION_MZONE)
+	if ft<=0 then return end
+	local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,math.min(2,ft),nil,e,tp)
+	if #g>0 then
+		Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		s.applylock(e,tp)
+	end
+end
+
+--------------------------------------------------
+-- Destroy Replacement (GY effect)
+--------------------------------------------------
+function s.repfilter(c)
+	return c:IsFaceup() and (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT))
+end
+
+function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then return c:IsAbleToRemove() end
+	return Duel.IsExistingMatchingCard(s.repfilter,tp,LOCATION_MZONE,0,1,nil)
+end
+
+function s.repval(e,c)
+	return c:IsControler(e:GetHandlerPlayer()) and c:IsFaceup()
+		and (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT))
+end
+
+function s.repop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Remove(e:GetHandler(),POS_FACEUP,REASON_EFFECT)
+	s.applylock(e,tp)
+end
+
+--------------------------------------------------
+-- Custom Lock Implementation (your version)
+--------------------------------------------------
+function s.actcon(e,tp,eg,ep,ev,re,r,rp)
+	return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0
+end
+
+function s.applylock(e,tp)
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c)
+		return not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT))
+	end)
+	e1:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e1,tp)
+end
---
+++ script/c901003.lua
@@ -0,0 +1,109 @@
+--Psychic Rose Knight
+--Scripted by Beanbag
+local s,id=GetID()
+
+function s.initial_effect(c)
+    --Also treated as Psychic
+    local e0=Effect.CreateEffect(c)
+    e0:SetType(EFFECT_TYPE_SINGLE)
+    e0:SetCode(EFFECT_ADD_RACE)
+    e0:SetValue(RACE_PSYCHIC)
+    c:RegisterEffect(e0)
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+	e1:SetCode(EFFECT_SYNCHRO_MAT_FROM_HAND)
+	e1:SetRange(LOCATION_HAND)
+	e1:SetCountLimit(1,{id,0})
+	e1:SetValue(s.synval)
+	c:RegisterEffect(e1)
+    function s.initial_effect(c)
+    --Ignition effect: reduce level and search
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_LVCHANGE+CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1,{id,1})
+    e2:SetTarget(s.lvtg)
+    e2:SetOperation(s.lvop)
+    c:RegisterEffect(e2)
+end
+end
+--========================
+-- KONAMI SUMMON LOCK CORE
+--========================
+Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,function(c)
+    return c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT)
+end)
+
+function s.actcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0
+end
+
+function s.applylock(e,tp)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(function(e,c) return not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT)) end)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+end
+--Psychic Rose Knight
+local s,id=GetID()
+
+-- archetype filter
+function s.filter(c)
+    return c:IsFaceup() and c:IsSetCard(0x1A0A) and not c:IsCode(id)
+end
+
+-- Spell/Trap search filter
+function s.thfilter(c)
+    return c:IsSetCard(0x1A0A) and c:IsType(TYPE_SPELL+TYPE_TRAP) and c:IsAbleToHand()
+end
+
+function s.lvtg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingTarget(s.filter,tp,LOCATION_MZONE,0,1,nil)
+        and Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK,0,1,nil)
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    local g=Duel.SelectTarget(tp,s.filter,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function s.lvop(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if not tc or not tc:IsRelateToEffect(e) or not tc:IsFaceup() then return end
+
+    -- Choose amount (1 to 3)
+    local lv=1
+    local op=Duel.SelectOption(tp,aux.Stringid(id,0),aux.Stringid(id,1),aux.Stringid(id,2)) -- 1,2,3
+    if op==1 then lv=2 end
+    if op==2 then lv=3 end
+
+    -- Apply level reduction
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UPDATE_LEVEL)
+    e1:SetValue(-lv)
+    e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+    tc:RegisterEffect(e1)
+
+    -- If successful, add a "Psychic Rose" Spell/Trap
+    if lv>0 then
+        Duel.BreakEffect()
+        Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+        local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+        if #g>0 then
+            Duel.SendtoHand(g,nil,REASON_EFFECT)
+            Duel.ConfirmCards(1-tp,g)
+         s.applylock(e,tp)
+        end
+    end
+end
+function s.synval(e,mc,sc) --this effect, this card and the monster to be summoned
+	return sc:IsType(TYPE_SYNCHRO) and sc:IsRace(RACE_PSYCHIC) or (sc:IsRace(RACE_PLANT))
+end
