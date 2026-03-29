b287fcc76624dd1cecb6a0a88362177f2b8243db
Spagroll-Customs <Joecollins@hotmail.co.uk>
2026-03-29 14:18:11 +0100

    Update c901001.lua

--- script/c901001.lua
+++ script/c901001.lua
@@ -9,15 +9,12 @@ function s.initial_effect(c)
     e0:SetCode(EFFECT_ADD_RACE)
     e0:SetValue(RACE_PSYCHIC)
     c:RegisterEffect(e0)
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_IGNITION)
+   local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e1:SetRange(LOCATION_HAND)
-    e1:SetCountLimit(1,{id,0})
     e1:SetCondition(s.spcon)
-    e1:SetTarget(s.sptg)
-    e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
     --On summon: search
     local e2=Effect.CreateEffect(c)
@@ -73,21 +70,15 @@ end
 -- SPECIAL SUMMON CONDITION
 --========================
 function s.csfilter(c)
-    return c:IsFacedown() or not c:IsSetCard(0x1A0A)
+    return not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT))
 end
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-    return not Duel.IsExistingMatchingCard(s.csfilter,tp,LOCATION_MZONE,0,1,nil)
-end
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and e:GetHandler():IsCanBeSpecialSummoned(e,0,tp,false,false) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,e:GetHandler(),1,0,0)
-end
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        Duel.SpecialSummon(c,0,tp,tp,false,false,POS_FACEUP)
-    end
+
+function s.spcon(e,c)
+    if c==nil then return true end
+    local tp=c:GetControler()
+    local g=Duel.GetFieldGroup(tp,LOCATION_MZONE,0)
+    return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and (#g==0 or not g:IsExists(s.csfilter,1,nil))
 end
 
 --========================
