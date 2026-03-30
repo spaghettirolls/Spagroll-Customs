6a8ecd27b58c01ea1c14ddc809b2e1852839800f
Spagroll-Customs <Joecollins@hotmail.co.uk>
2026-03-30 14:56:08 +0100

    update

---
+++ script/c901006.lua
@@ -0,0 +1,130 @@
+--Gaze of the Psychic Rose
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
+    local e1=Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1,id)
+    e1:SetCondition(s.actcon)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.activate)
+    c:RegisterEffect(e1)
+
+    --GY effect
+    local e2=Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DRAW+CATEGORY_HANDES)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1,id+100)
+    e2:SetCondition(s.actcon)
+    e2:SetCost(s.gycost)
+    e2:SetTarget(s.gytg)
+    e2:SetOperation(s.gyop)
+    c:RegisterEffect(e2)
+end
+
+--Filters
+function s.psyrose_filter(c)
+    return c:IsFaceup() and c:IsSetCard(0x1A0A)
+end
+
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0x1A0A) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.synfilter(c)
+    return c:IsType(TYPE_SYNCHRO) and (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT)) and c:IsAbleToRemoveAsCost()
+end
+
+--Activation Target
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return Duel.IsExistingTarget(s.psyrose_filter,tp,LOCATION_MZONE,0,1,nil)
+    end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TARGET)
+    local g=Duel.SelectTarget(tp,s.psyrose_filter,tp,LOCATION_MZONE,0,1,1,nil)
+    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_REMOVED)
+end
+
+--Activation Operation
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
+    local tc=Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        --Make Tuner
+        local e1=Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_ADD_TYPE)
+        e1:SetValue(TYPE_TUNER)
+        e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+        tc:RegisterEffect(e1)
+
+        --Special Summon
+        if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+            and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_REMOVED,0,1,nil,e,tp)
+            and Duel.SelectYesNo(tp,aux.Stringid(id,0)) then
+            
+            Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+            local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_REMOVED,0,1,1,nil,e,tp)
+            if #g>0 then
+                Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+                    s.applylock(e,tp)
+            end
+        end
+    end
+end
+
+
+--GY Cost
+function s.gycost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then
+        return aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,0)
+            and Duel.IsExistingMatchingCard(s.synfilter,tp,LOCATION_GRAVE,0,1,e:GetHandler())
+    end
+    aux.bfgcost(e,tp,eg,ep,ev,re,r,rp,1)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_REMOVE)
+    local g=Duel.SelectMatchingCard(tp,s.synfilter,tp,LOCATION_GRAVE,0,1,1,e:GetHandler())
+    Duel.Remove(g,POS_FACEUP,REASON_COST)
+end
+
+--GY Target
+function s.gytg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+    Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+    Duel.SetOperationInfo(0,CATEGORY_HANDES,nil,0,tp,1)
+end
+
+--GY Operation
+function s.gyop(e,tp,eg,ep,ev,re,r,rp)
+    if Duel.Draw(tp,2,REASON_EFFECT)>0 then
+        Duel.ShuffleHand(tp)
+        Duel.BreakEffect()
+        Duel.DiscardHand(tp,aux.TRUE,1,1,REASON_EFFECT+REASON_DISCARD)
+        s.applylock(e,tp)
+end
+--========================
+-- KONAMI SUMMON LOCK CORE
+--========================
+Duel.AddCustomActivityCounter(id,ACTIVITY_SPSUMMON,function(c)
+    return c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT)
+end)
+
+function s.actcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.GetCustomActivityCount(id,tp,ACTIVITY_SPSUMMON)==0
+end
+
+function s.applylock(e,tp)
+    local e1=Effect.CreateEffect(e:GetHandler())
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+    e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    e1:SetTargetRange(1,0)
+    e1:SetTarget(function(e,c) return not (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT)) end)
+    e1:SetReset(RESET_PHASE+PHASE_END)
+    Duel.RegisterEffect(e1,tp)
+    -- Client hint (THIS is what shows under the username)
+    aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,
+        aux.Stringid(id,0),
+        nil)
+end
