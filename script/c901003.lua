68df8dbd23afc5b5871f9b472025f1eb2d108ca8
Spagroll-Customs <Joecollins@hotmail.co.uk>
2026-03-30 16:44:06 +0100

    Update c901003.lua

--- script/c901003.lua
+++ script/c901003.lua
@@ -11,7 +11,6 @@ function s.initial_effect(c)
     c:RegisterEffect(e0)
     --Ignition effect: reduce level and search
     local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
     e2:SetCategory(CATEGORY_LVCHANGE+CATEGORY_TOHAND+CATEGORY_SEARCH)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_MZONE)
@@ -19,6 +18,16 @@ function s.initial_effect(c)
     e2:SetTarget(s.lvtg)
     e2:SetOperation(s.lvop)
     c:RegisterEffect(e2)
+    local e3=Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
+    e3:SetCode(EVENT_TO_GRAVE)
+    e3:SetCountLimit(1,{id,2})
+    e3:SetCondition(s.descon)
+    e3:SetTarget(s.destg)
+    e3:SetOperation(s.desop)
+    c:RegisterEffect(e3)
     local e5=Effect.CreateEffect(c)
     e5:SetType(EFFECT_TYPE_SINGLE)
     e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
@@ -107,4 +116,30 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 end
 function s.synval(e,mc,sc) --this effect, this card and the monster to be summoned
     return sc:IsType(TYPE_SYNCHRO) and sc:IsRace(RACE_PSYCHIC) or (sc:IsRace(RACE_PLANT))
+end
+function s.descon(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local rc=c:GetReasonCard()
+    return c:IsReason(REASON_SYNCHRO)
+        and rc
+        and (rc:IsRace(RACE_PSYCHIC) or rc:IsRace(RACE_PLANT))
+end
+
+function s.desfilter(c)
+    return c:IsFaceup() and c:IsType(TYPE_SYNCHRO)
+        and (c:IsRace(RACE_PSYCHIC) or c:IsRace(RACE_PLANT))
+end
+
+function s.destg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local ct=Duel.GetMatchingGroupCount(s.desfilter,tp,LOCATION_MZONE,0,nil)
+    if chk==0 then return ct>0 and Duel.IsExistingTarget(aux.TRUE,tp,0,LOCATION_ONFIELD,1,nil) end
+    local g=Duel.SelectTarget(tp,aux.TRUE,tp,0,LOCATION_ONFIELD,1,ct,nil)
+    Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,#g,0,0)
+end
+
+function s.desop(e,tp,eg,ep,ev,re,r,rp)
+    local g=Duel.GetTargetCards(e)
+    if #g>0 then
+        Duel.Destroy(g,REASON_EFFECT)
+    end
 end
